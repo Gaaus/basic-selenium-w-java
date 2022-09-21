cbbcddd2d12cd5f0d5077d6e1d3b7ac82ca28295
Gaaus17 <Gaaus17@gmail.com>
2022-09-21 11:07:57 -0700

    update

--- src/lesson_01/VariableLearning.java
+++ src/lesson_01/VariableLearning.java
@@ -6,7 +6,14 @@ public class VariableLearning {
         System.out.println(Byte.MAX_VALUE);
         System.out.println(Byte.MIN_VALUE);
         short myIntNum = 129;
+        long myLongNum = 2000;
+        float myFloatNum = 3.14f; // float luon co' datatype suffix, other num datatype no need
+        double myDoubleNum = 3.14D;
         System.out.println(myIntNum);
+
+        // constant declaring variable, often using naming convention as Cap snake_case: MY_CONSTANT
+        final int MY_CONSTANT = 123;
+        // MY_CONSTANT = 124; // java does not allow to assign new value for a constant variable
 
     }
 }
