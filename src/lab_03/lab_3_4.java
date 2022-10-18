63ba6cecf9312f33b6a93d7bf35b4d450c43536e
Gaaus17 <Gaaus17@gmail.com>
2022-10-18 12:29:30 -0700

    update & fix based on mentor's solutions in Video Lesson 05

--- src/lab_03/lab_3_4.java
+++ src/lab_03/lab_3_4.java
@@ -6,39 +6,86 @@ import java.util.List;
 
 public class lab_3_4 {
     public static void main(String[] args) {
-        int[] intArr01 = {1,12,16,28,34};
+        int[] intArr01 = {1,12,12,16,28,34};
         int[] intArr02 = {1,13,16,27,99};
 
-        List<Integer> resultArr = new ArrayList<>();
+        // pointer ind for arr1, arr2, mergedArr.
+        // while pointer 1 < arr1 length && pointer 2 < arr2 length
+        //      compare and pick small number, increase idx of arr has the picked small number
 
-        int point1;
-        if (intArr01.length <= intArr02.length) {
-            point1 = intArr01.length;
-        } else {
-            point1 = intArr02.length;
-        }
+        int lengthArr01 = intArr01.length;
+        int lengthArr02 = intArr02.length;
+        int[] mergedArr = new int[lengthArr01 + lengthArr02];
 
-        for (int i = 0; i < point1; i++) {
-            if (intArr01[i] <= intArr02[i]) {
-                resultArr.add(intArr01[i]);
-                resultArr.add(intArr02[i]);
+        int pointer1, pointer2, pointer3;
+        pointer1 = pointer2 = pointer3 = 0;
+
+        while (pointer1 < lengthArr01 && pointer2 < lengthArr02) {
+            if (intArr01[pointer1] <= intArr02[pointer2]) {
+                mergedArr[pointer3] = intArr01[pointer1];
+                pointer1++;
             } else {
-                resultArr.add(intArr02[i]);
-                resultArr.add(intArr01[i]);
+                mergedArr[pointer3] = intArr02[pointer2];
+                pointer2++;
             }
+            pointer3++;
+
         }
 
-        // if 2 origin arrays' length are not equal, add the rest sorted array to result
-        if (intArr01.length < intArr02.length) {
-            for (int k = point1; k < intArr02.length ; k++) resultArr.add(intArr02[k]);
-        }
-        if (intArr01.length > intArr02.length) {
-            for (int k = point1; k < intArr01.length; k++) resultArr.add(intArr01[k]);
+        if (pointer1 < lengthArr01) {
+            for (int idx = pointer1; idx < lengthArr01 ; idx++) {
+                mergedArr[pointer3] = intArr01[idx];
+                pointer3++;
+            }
+
         }
 
-        System.out.println("The 1st sorted integer array is: " + Arrays.toString(intArr01));
-        System.out.println("The 2nd sorted integer array is: " + Arrays.toString(intArr02));
-        System.out.print("Result of merging 2 sorted arrays is: ");
-        System.out.println(resultArr);
+        if (pointer2 < lengthArr02) {
+            for (int idx = pointer2; idx < lengthArr02 ; idx++) {
+                mergedArr[pointer3] = intArr02[idx];
+                pointer3++;
+            }
+
+        }
+
+        System.out.println("Arr1 is: " + Arrays.toString(intArr01));
+        System.out.println("Arr2 is: " + Arrays.toString(intArr02));
+        System.out.println();
+        System.out.println("Merged array from the 2 sorted arrays is: " + Arrays.toString(mergedArr));
+
+
+
+//      Wrong output when arr1 has duplicated number that less than arr2. see above
+//        List<Integer> resultArr = new ArrayList<>();
+//
+//        int point1;
+//        if (intArr01.length <= intArr02.length) {
+//            point1 = intArr01.length;
+//        } else {
+//            point1 = intArr02.length;
+//        }
+//
+//        for (int i = 0; i < point1; i++) {
+//            if (intArr01[i] <= intArr02[i]) {
+//                resultArr.add(intArr01[i]);
+//                resultArr.add(intArr02[i]);
+//            } else {
+//                resultArr.add(intArr02[i]);
+//                resultArr.add(intArr01[i]);
+//            }
+//        }
+//
+//        // if 2 origin arrays' length are not equal, add the rest sorted array to result
+//        if (intArr01.length < intArr02.length) {
+//            for (int k = point1; k < intArr02.length ; k++) resultArr.add(intArr02[k]);
+//        }
+//        if (intArr01.length > intArr02.length) {
+//            for (int k = point1; k < intArr01.length; k++) resultArr.add(intArr01[k]);
+//        }
+//
+//        System.out.println("The 1st sorted integer array is: " + Arrays.toString(intArr01));
+//        System.out.println("The 2nd sorted integer array is: " + Arrays.toString(intArr02));
+//        System.out.print("Result of merging 2 sorted arrays is: ");
+//        System.out.println(resultArr);
     }
 }
--- src/lesson_02/BitwiseOperator.java
+++ src/lesson_02/BitwiseOperator.java
@@ -2,28 +2,30 @@ package lesson_02;
 
 public class BitwiseOperator {
     public static void main(String[] args) {
-        // && ca 2 dk deu cung dung, dk 1 sai thi khong thuc thi dk 2, always false
-        //  || 1 trong 2 dk dung' thi thuc thi, hoac chi can dk 1 dung' thi da thuc thi roi!!!
-        // neu chi 1 dau & hoac 1 dau | thi 2 dk luon duoc thuc thi, roi moi process tiep
-        if (getFirstNum() != 0 && getSecondNum() != 0) {
+        // && ca 2 dk deu cung dung, dk 1 sai thi khong thuc thi dk 2, always false to execute IF statement
+        // condition A && condition B: Both conditions must be True to execute IF statement.
+        // Check from L->R, if condition A False, won't care B, any False wont execute IF statement
+        // if condition A true, continue validate B
+
+        // || chi can 1 trong 2 dk dung thi thuc thi IF statement
+        // condition A || condition B ==> only if condition A False, then continue validate condition B
+        // if condition A True, won't care B
+        // Only won't execute IF statement when Both A , B are False
+
+        if (getFirstNum() != 0 || getSecondNum() != 0) {
             System.out.println("Calculating....");
+            System.out.println(8 &10);
         }
     }
 
         private static int getFirstNum() {
             System.out.println("Calling getting 1st num!");
-            // return 0; --> test 1: "Calling getting 1st num!" only
-            // return 1; // test 2:  "Calling...1st" & Calling...2nd & Calculating
-            return 1;
+            return 0;
         }
 
         private static int getSecondNum() {
             System.out.println("Calling getting 2nd num!");
-            // return 2; // test 2: "Calling...1st" & Calling...2nd & Calculating
-            return 0; // test 3: "calling ... 1st" & Calling ...2nd - no Calculating... since criteria 2 is false
+            return 0;
         }
-
-        // neu if (getFirstNum() != 0 & getSecondNum() != 0) ==> 1 dau & hoac 1 dau |,  thi luon evaluate ca 2 crits calling...1st, 2nd
-        // neu ||, crit 1st sai thi moi evaluate (xem xet) toi crit 2
 
 }
