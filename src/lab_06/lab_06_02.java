655bdf0600b0a29ef67c7226ce7dee74dcf8b4d0
Gaaus17 <Gaaus17@gmail.com>
2022-11-16 16:48:27 -0800

    update lab 6.2

---
+++ src/lab_06/lab_06_02.java
@@ -0,0 +1,49 @@
+package lab_06;
+
+import java.util.Scanner;
+
+public class lab_06_02 {
+    public static void main(String[] args) {
+        /* Check user input correct password, allow to input maximum 3 times
+        * quota = 3
+        * parse char in myPassword to myArrPass
+        * * do (while quota > 0):
+            * usr input pass
+            * parse char in usrPassword to usrArrPass
+            * If myArrPass.length = usrArrPass.length:
+            *   no: -> Failed
+            *   pass: loop over char of myArrPass:
+            *       if myArrPass[i].equals(usrArrPass[i]) --> Pass
+            *       else" failed
+            * quota--;
+        * */
+        String myPassword = "password123";
+        int quota = 3;
+        boolean checkResult = false;
+
+        do {
+            System.out.print("Please input password...");
+            Scanner scanner = new Scanner(System.in);
+            String usrPassword = scanner.nextLine();
+
+            checkResult = checkPassword(usrPassword, myPassword);
+            if (checkResult) {
+                System.out.println("Congrats... Welcome to Dashboard!");
+                quota = 0;
+            } else {
+                System.out.println("Wrong password...");
+                quota--;
+                if (quota == 0) System.out.println("No input accepted anymore..Bye bye...!!!");
+            }
+        } while (quota > 0);
+
+    }
+
+    private static boolean checkPassword(String usrPassword, String myPassword) {
+        if (myPassword.length() != usrPassword.length()) return false;
+        else {
+            if (myPassword.equals(usrPassword) == true) return true;
+            else return false;
+        }
+    }
+}
